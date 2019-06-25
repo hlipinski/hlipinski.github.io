84668e065b85e039f269375ff37052e710d7dd60
Hubert Lipiński <hlp@touk.pl>
2019-06-25 22:49:56 +0200

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-b7797fe1535f8e31ee9a.js"
+    "url": "webpack-runtime-df0214484483ca72d29a.js"
   },
   {
     "url": "styles.44a1025fc8a56fd68cb3.css"
@@ -42,7 +42,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "32380cb252c671142ab52552a1ffad2e"
+    "revision": "211334ee7ecab2b8469227b8d8b8445e"
   },
   {
     "url": "component---src-pages-404-js-baf8fafbe1e26da009b4.js"
